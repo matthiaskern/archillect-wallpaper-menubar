b4ec723085393da1ce7c1a4930e7f6ad0df5d425
Matthias Kern <matthias_kern@posteo.net>
2017-08-11 11:36:51 +0200

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const {app, shell, Menu, Tray} = require('electron');
+
+const iconPath = path.join(__dirname, 'assets/iconTemplate.png');
+
+const changeWallpaper = require('archillect-wallpaper')
+
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: 'Feedback',
+    type: 'normal',
+    click() {
+      shell.openExternal('https://github.com/matthiaskern/archillect-wallpaper-menubar');
+    }
+  },
+  {
+    type: 'separator'
+  },
+  {
+    label: 'Quit',
+    type: 'normal',
+    role: 'quit'
+  }
+]);
+
+
+app.on('ready', () => {
+  app.dock.hide();
+
+  const tray = new Tray(iconPath);
+
+  tray.setToolTip('Archillect');
+
+  tray.on('click', () => changeWallpaper());
+  tray.on('right-click', () => tray.popUpContextMenu(contextMenu));
+});
